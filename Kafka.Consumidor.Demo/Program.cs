7fa9c89e0a11356591e8cc4e1e43801439fea54e
Nathay Mesquita <nnathalygm@gmail.com>
2022-01-14 00:50:58 -0300

    feat(): Projeto base para demo da palestra

---
+++ Kafka.Consumidor.Demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Serilog;
+using Confluent.Kafka;
+
+namespace Kafka.Consumidor.Demo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Information("Executando um Consumidor do Kafka com C#");
+
+            var config = new ConsumerConfig
+            {
+                GroupId = "BUSQUEAKI",
+                BootstrapServers = "localhost:9092"
+            };
+            Log.Information($"Configurações do consumidor - GroupID:'{config.GroupId}' e  Broker:'{config.BootstrapServers}'");
+
+            using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            consumer.Subscribe("Nova_Consulta");
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume();
+                        Log.Information($"Mensagem consumida é '{consumeResult.Message.Value}' do Topico:'{consumeResult.Topic}' na partition:'{consumeResult.Partition}' e offset:'{consumeResult.Offset}' ");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Log.Error($"Error occured: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Log.Information("Garante que o consumidor deixou o grupo limpo e o commit foi feito nos offsets finais");
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ Kafka.Produtor.Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Serilog;
+using Confluent.Kafka;
+using System.Threading.Tasks;
+
+namespace Kafka.Produtor.Demo
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            Log.Information("Executando um Produtor de Kafka com C#");
+
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            Log.Information($"Configurações do produtor - Broker:'{config.BootstrapServers}'");
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var deliveryResult = await producer.ProduceAsync("Nova_Consulta", new Message<Null, string> { Value = "Computador gamer" });
+                    Log.Information($"Entrega de '{deliveryResult.Value}' para o Topico:'{deliveryResult.Topic}' partition:'{deliveryResult.Partition}' e offset:'{deliveryResult.Offset}' ");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Log.Information($"Entrega Falhou: {e.Error.Reason}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
